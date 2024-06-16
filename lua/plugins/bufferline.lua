c01e61b4f45d1d40e7615c58f3013ff16ca246b9
Jacquin Moon <jq.nhi2306@gmail.com>
2024-06-16 19:05:49 +0700

    customize bufferline

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,91 +1,150 @@
+local function is_ft(b, ft)
+  return vim.bo[b].filetype == ft
+end
+
+local function custom_filter(buf, buf_nums)
+  local logs = vim.tbl_filter(function(b)
+    return is_ft(b, "log")
+  end, buf_nums or {})
+  if vim.tbl_isempty(logs) then
+    return true
+  end
+  local tab_num = vim.fn.tabpagenr()
+  local last_tab = vim.fn.tabpagenr("$")
+  local is_log = is_ft(buf, "log")
+  if last_tab == 1 then
+    return true
+  end
+  -- only show log buffers in secondary tabs
+  return (tab_num == last_tab and is_log) or (tab_num ~= last_tab and not is_log)
+end
+
 return {
   {
     "akinsho/bufferline.nvim",
-    event = "UIEnter",
     dependencies = { "nvim-tree/nvim-web-devicons" },
+    event = "UIEnter",
     opts = {
+      highlights = {
+        background = { italic = true },
+        buffer_selected = { bold = true },
+      },
       options = {
+        themable = true,
         close_command = function(n)
           LazyVim.ui.bufremove(n)
         end,
         right_mouse_command = "vert sbuffer %d",
         left_mouse_command = "buffer %d",
         diagnostics = false,
-        always_show_bufferline = true,
-        auto_toggle_bufferline = true,
-        show_duplicate_prefix = true,
-        duplicates_across_groups = true,
-        move_wraps_at_ends = false,
-        groups = {
-          items = {
-            require("bufferline").groups.builtin.pinned:with({ icon = "" }),
-            require("bufferline").groups.builtin.ungrouped,
-          },
-          options = { toggle_hidden_on_enter = true },
-        },
-        themeable = true,
+        mode = "buffers",
         numbers = "none",
-        separator_style = { "▌", "▌" },
-        indicator = { style = "slope" },
-        hover = {
-          enabled = true,
-          delay = 100,
-          reveal = { "close" },
-        },
-        sort_by = "id",
-        debug = { logging = false },
+        custom_filter = custom_filter,
         buffer_close_icon = "󰅖",
         modified_icon = "●",
         close_icon = "",
         left_trunc_marker = "",
         right_trunc_marker = "",
-        max_name_length = 18,
-        max_prefix_length = 15,
-        truncate_names = true,
-        tab_size = 18,
+        offsets = {
+          {
+            filetype = "neo-tree",
+            text = " EXPLORER",
+            highlight = "Directory",
+            text_align = "left",
+            padding = 1,
+          },
+          {
+            filetype = "undotree",
+            text = " UNDOTREE",
+            highlight = "PanelHeading",
+            padding = 1,
+          },
+          {
+            text = "󰆼 DATABASE VIEWER",
+            filetype = "dbui",
+            highlight = "PanelHeading",
+            separator = true,
+          },
+          {
+            filetype = "DiffviewFiles",
+            text = " DIFF VIEW",
+            highlight = "PanelHeading",
+            padding = 1,
+          },
+          {
+            filetype = "lazy",
+            text = " LAZY",
+            highlight = "PanelHeading",
+            padding = 1,
+          },
+        },
         color_icons = true,
+        get_element_icon = function(opts)
+          return LazyVim.config.icons.ft[opts.filetype]
+        end,
         show_buffer_icons = true,
         show_buffer_close_icons = true,
         show_close_icon = false,
         show_tab_indicators = true,
-        persist_buffer_sort = true,
         enforce_regular_tabs = false,
-        offsets = {
-          {
-            filetype = "neo-tree",
-            text = " EXPLORER",
-            highlight = "PanelHeading",
-            text_align = "left",
-            separator = true,
+        persist_buffer_sort = true,
+        separator_style = "slant",
+        indicator = { style = "underline" },
+        style_preset = { require("bufferline").style_preset.minimal },
+        always_show_bufferline = true,
+        hover = { enabled = true, delay = 100, reveal = { "close" } },
+        sort_by = "id",
+        debug = { logging = false },
+        max_name_length = 18,
+        max_prefix_length = 15, -- prefix used when a buffer is de-duplicated
+        truncate_names = true, -- whether or not tab names should be truncated
+        tab_size = 18,
+        groups = {
+          options = { toggle_hidden_on_enter = true },
+          items = {
+            require("bufferline").groups.builtin.pinned:with({ icon = "" }),
+            require("bufferline").groups.builtin.ungrouped,
           },
           {
-            text = "UNDOTREE",
-            filetype = "undotree",
-            highlight = "PanelHeading",
-            separator = true,
+            name = "Dependencies",
+            icon = "",
+            highlight = { fg = "#ECBE7B" },
+            matcher = function(buf)
+              return vim.startswith(buf.path, vim.env.VIMRUNTIME)
+            end,
           },
           {
-            text = " DIFF VIEW",
-            filetype = "DiffviewFiles",
-            highlight = "PanelHeading",
-            separator = true,
+            name = "SQL",
+            matcher = function(buf)
+              return buf.name:match("%.sql$")
+            end,
           },
           {
-            filetype = "lazy",
-            text = "💭 LAZY",
-            highlight = "PanelHeading",
-            separator = true,
+            name = "Tests",
+            icon = "",
+            matcher = function(buf)
+              return buf.name:match("[_%.]spec") or buf.name:match("[_%.]test")
+            end,
+          },
+          {
+            name = "Docs",
+            icon = "",
+            matcher = function(buf)
+              if vim.bo[buf.id].filetype == "man" or buf.path:match("man://") then
+                return true
+              end
+              for _, ext in ipairs({ "md", "txt", "org", "norg", "wiki" }) do
+                if ext == vim.fn.fnamemodify(buf.path, ":e") then
+                  return true
+                end
+              end
+            end,
           },
         },
-        get_element_icon = function(element)
-          local icon, hl = require("nvim-web-devicons").get_icon_by_filetype(element.filetype, { default = false })
-          return icon, hl
-        end,
       },
     },
     config = function(_, opts)
       require("bufferline").setup(opts)
-      -- Fix bufferline when restoring a session
       vim.api.nvim_create_autocmd({ "BufAdd", "BufDelete" }, {
         callback = function()
           vim.schedule(function()
@@ -93,6 +152,7 @@ return {
           end)
         end,
       })
+      vim.opt.showtabline = 2
     end,
   },
 }
