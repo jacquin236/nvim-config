9e5a226d04891be3ccc47f3ff875c579077c85b6
Jacquin Moon <bumbabee@DESKTOP-913PQCH>
2024-06-19 19:57:25 +0700

    update

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -27,24 +27,19 @@ return {
   },
   {
     "uga-rosa/ccc.nvim",
-    cmd = { "CccPick" },
+    cmd = "CccPick",
     keys = {
       { "<leader>co", "<cmd>CccPick<cr>", mode = "n", desc = "Open CCC Color Picker" },
     },
     config = function()
-      ---@diagnostic disable: no-unknown
-      local ccc = require("ccc")
       local ColorInput = require("ccc.input")
-      local utils = require("ccc.utils")
-      local convert = utils.convert
-
-      local mapping = ccc.mapping
+      local convert = require("ccc.utils.convert")
 
       local RgbHslInput = setmetatable({
         name = "RGB/HSL",
         max = { 1, 1, 1, 360, 1, 1 },
         min = { 0, 0, 0, 0, 0, 0 },
-        delta = { 1 / 255, 1 / 255, 1 / 255, 1, 0.01, 0, 01 },
+        delta = { 1 / 255, 1 / 255, 1 / 255, 1, 0.01 },
         bar_name = { "R", "G", "B", "H", "S", "L" },
       }, { __index = ColorInput })
 
@@ -54,7 +49,6 @@ return {
           -- RGB
           n = n * 255
         elseif i == 5 or i == 6 then
-          -- S or L of HSL
           n = n * 100
         end
         return ("%6d"):format(n)
@@ -62,23 +56,28 @@ return {
 
       function RgbHslInput.from_rgb(RGB)
         local HSL = convert.rgb2hsl(RGB)
-        local R, G, B = unpack(RGB)
+        local R, G, B = unpack(RGB) 
         local H, S, L = unpack(HSL)
         return { R, G, B, H, S, L }
       end
 
-      function RgbHslInput:_set_rgb(RGB)
+      function RgbHslInput.to_rgb(value)
+        return { value[1], value[2], value[3] }
+      end
+      
+      ---@diagnostic disable: undefined-global, no-unknown
+      function RgbHslInput._set_rgb(RGB)
         self.value[1] = RGB[1]
         self.value[2] = RGB[2]
         self.value[3] = RGB[3]
       end
 
-      function RgbHslInput:_set_hsl(HSL)
+      function RgbHslInput._set_hsl(HSL)
         self.value[4] = HSL[1]
         self.value[5] = HSL[2]
         self.value[6] = HSL[3]
       end
-
+      
       ---@diagnostic disable-next-line: duplicate-set-field
       function RgbHslInput:callback(index, new_value)
         self.value[index] = new_value
@@ -87,26 +86,31 @@ return {
           local RGB = { v[1], v[2], v[3] }
           local HSL = convert.rgb2hsl(RGB)
           self:_set_hsl(HSL)
-        else
+        else 
           local HSL = { v[4], v[5], v[6] }
           local RGB = convert.hsl2rgb(HSL)
-          self:_set_rgb(RGB)
+          self:_set_rgb(RGB) 
         end
       end
 
-      local FuncRgbOutput = { name = "FuncRgb" }
-      function FuncRgbOutput:str(RGB, A)
-        local R, G, B = convert.rgb_format(RGB)
+      local FuncRgbOutput = { name = "FuncRGB" }
+      local utils = require("ccc.utils")
+
+      function FuncRgbOutput.str(RGB, A)
+        local R, G, B = convert.rgb_format(RGB) 
         R = utils.round(R)
         G = utils.round(G)
         B = utils.round(B)
-        if A then
-          A = utils.round(A * 100)
+        if A then 
+        A = utils.round(A * 100) 
           return ("rgba(%d, %d, %d, %d)"):format(R, G, B, A)
-        else
-          return ("rgb(%d, %d, %d)"):format(R, G, B)
+        else 
+          return("rgb(%d, %d, %d)"):format(R, G, B)
         end
       end
+
+      local ccc = require("ccc")
+      local mapping = require("ccc.mapping")
 
       ccc.setup({
         inputs = { RgbHslInput },
