b94cac000caedfdf2a0eb03b8a0432ee8df87ad9
Jacquin Moon <jq.nhi2306@gmail.com>
2024-07-22 11:32:56 +0700

    fix typos and additional keymaps

--- lua/modules/configs/lang/lsp/extras/package-info.lua
+++ lua/modules/configs/lang/lsp/extras/package-info.lua
@@ -8,11 +8,13 @@ return {
     },
     keys = {
       -- stylua: ignore start
-      { "<leader>pwv", function() require("package-info").show({ force = true }) end, desc = "Package Versions" },
+      { "<leader>pwv", function() require("package-info").show({ force = true }) end, desc = "Show Dependencies Versions" },
+      { "<leader>pwV", function() require("package-info").hide() end, desc = "Hide Dependencies Versions" },
       { "<leader>pwu", function() require("package-info").update() end, desc = "Update Package" },
       { "<leader>pwr", function() require("package-info").delete() end, desc = "Remove Package" },
       { "<leader>pwc", function() require("package-info").change_version() end, desc = "Change Package Version" },
       { "<leader>pwi", function() require("package-info").install() end, desc = "Install New Dependency" },
+      { "<leader>pwp", function() require("package-info").toggle() end, desc = "Toggle Package-Info" },
       { "<leader>pwt", "<cmd>Telescope package_info<cr>", desc = "Package Info" },
       -- stylua: ignore end
     },
